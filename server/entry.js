2fc77cdea66e8b96a94c77745404490f3e96d2ba
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-12 22:18:35 -0400

    Updated Webpack to use common.web.js

--- server/entry.js
+++ server/entry.js
@@ -3,5 +3,8 @@ require('./todo-subscriptions');
 
 // Do server-rendering only in proudction mode
 if (process.env.NODE_ENV === 'production') {
+  // Load Webpack infos for SSR
+  ReactRouterSSR.LoadWebpackStats(JSON.parse(Assets.getText('webpack.stats.json')));
+  
   require('../client/routes');
 }
