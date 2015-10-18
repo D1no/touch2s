ab467402b0b41d03488845c501edec67585ca599
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-18 14:50:01 -0400

    Update webpack

--- entry/server/entry.js
+++ entry/server/entry.js
@@ -7,7 +7,7 @@ import 'TodoApp/server/todo-subscriptions'
 // Do server-rendering only in proudction mode
 if (process.env.NODE_ENV === 'production') {
   // Load Webpack infos for SSR
-  ReactRouterSSR.LoadWebpackStats(JSON.parse(Assets.getText('webpack.stats.json')));
+  ReactRouterSSR.LoadWebpackStats(WebpackStats);
 
   require('../client/routes');
 }
