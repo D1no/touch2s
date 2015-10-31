cf06206a6f661ccb5859941b486ece488c65da20
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-31 06:37:28 -0400

    Update externals

--- entry/webpack.conf.js
+++ entry/webpack.conf.js
@@ -5,7 +5,13 @@ module.exports = {
   externals: {
     // Make sure we use Meteor package for react and react-router
     'react': 'React',
-    'react-router': 'ReactRouter'
+    'react-router': 'ReactRouter',
+    'react-dom': 'ReactDOM',
+    'react-addons-transition-group': 'React.addons.TransitionGroup',
+    'react-addons-create-fragment': 'React.addons.createFragment',
+    'react-addons-pure-render-mixin': 'React.addons.PureRenderMixin',
+    'react-addons-update': 'React.addons.update',
+    'react-addons-linked-state-mixin': 'React.addons.PureRenderMixin'
   },
   devServer: {
     // You can change this to your server IP address to access it remotely
