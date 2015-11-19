c15516d8d4b1d4e424373afdfd29b950b4dd7d17
Benoit Tremblay <trembl.ben@gmail.com>
2015-11-19 05:58:36 -0500

    Add hot-middleware reload option

--- entry/webpack.conf.js
+++ entry/webpack.conf.js
@@ -19,6 +19,9 @@ module.exports = {
     // You can change this to your server IP address to access it remotely
     host: 'localhost'
   },
+  hotMiddleware: {
+    reload: true
+  },
   resolve: {
     root: path.join(__dirname, '..', 'modules'),
     extensions: ['', '.js', '.jsx', '.json', '.css', '.scss']
