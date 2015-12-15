3584ee3979dab89b7ee8cb95ac6df3696150a876
Benoit Tremblay <trembl.ben@gmail.com>
2015-12-15 18:14:59 -0500

    Update packages + use react-helmet

--- modules/TodoApp/client/TodoApp.jsx
+++ modules/TodoApp/client/TodoApp.jsx
@@ -1,4 +1,5 @@
 import { Component, PropTypes } from 'react';
+import Helmet from 'react-helmet';
 
 export default class TodoApp extends Component {
   static propTypes = {
@@ -8,6 +9,13 @@ export default class TodoApp extends Component {
   render() {
     return (
       <div>
+        <Helmet
+          title="Todo App"
+          meta={[
+            { name: 'description', content: 'This is a Todo application!' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+          ]}
+        />
         {this.props.children}
       </div>
     );
