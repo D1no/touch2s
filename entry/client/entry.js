ec2b0dd42b8b0ab156af1b54048af33538b62bd3
Dino Scheidt <github@dino-scheidt.de>
2015-12-30 03:21:54 +0100

    Add comments to f7 implementation

--- entry/client/entry.js
+++ entry/client/entry.js
@@ -1,6 +1,14 @@
 // Import Framework7
 import "TodoApp/client/f7/less/ios/framework7.ios.less";
 import "TodoApp/client/f7/less/ios/framework7.ios.colors.less";
+/* Framework7 Custom Build
+ * Framework7 comes with an own router and state management which would conflict with react router. So we need to excl.
+ * it. Luckily we can do that very simple by doing this with the master repo:
+ * https://github.com/nolimits4web/Framework7#custom-build
+ * For now this one is
+ * gulp custom -cards,accordion,searchbar,modals,autocomplete,progressbar,swipeout,sortable,smart-select,virtual-list,pull-to-refresh,infinite-scroll,scroll-toolbars,tabs,fast-clicks,swiper,picker,calendar,notifications,searchbar,modals,modals
+ */
+// add *.es5.js so the file so meteor ecmascript package ignores this (huge) file.
 import "TodoApp/client/f7/js/framework7.custom.es5.js";
 
 import "TodoApp/client/f7/less/my-app.less";
--- modules/TodoApp/client/f7/js/f7.js
+++ modules/TodoApp/client/f7/js/f7.js
@@ -1,18 +1,16 @@
-/**
- * Created by dinos on 27/12/15.
- */
+// The import of the library happens in client/entry.js to be sure wepbacks optimization does not kill our global
 
-
-// Initialize f7 via a static class to be sure we always deal with one instance.
+// Initialize f7 via a static class to be sure we always deal with one instance and don't conflict with SSR
 let Instance;
 export default class F7 {
   constructor(options) {
     const defaultOptions = {
       animateNavBackIcon:true
-    }
+    };
 
     this.options = options ? options : defaultOptions;
 
+    // Deal with SSR
     if(!Instance && Meteor.isClient) {
       Instance = new window.Framework7(this.options);
     }
