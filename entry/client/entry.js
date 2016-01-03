387bb5485050108f0cd0155d83b1d80d3e498807
Dino Scheidt <github@dino-scheidt.de>
2016-01-03 19:38:15 +0100

    Move styles to f7 class

--- entry/client/entry.js
+++ entry/client/entry.js
@@ -1,6 +1,3 @@
-// Import Framework7
-import "TodoApp/client/f7/less/ios/framework7.ios.less";
-import "TodoApp/client/f7/less/ios/framework7.ios.colors.less";
 /* Framework7 Custom Build
  * Framework7 comes with an own router and state management which would conflict with react router. So we need to excl.
  * it. Luckily we can do that very simple by doing this with the master repo:
@@ -10,8 +7,6 @@ import "TodoApp/client/f7/less/ios/framework7.ios.colors.less";
  */
 // add *.es5.js so the file so meteor ecmascript package ignores this (huge) file.
 import "TodoApp/client/f7/js/framework7.custom.es5.js";
-
-import "TodoApp/client/f7/less/my-app.less";
 
 // Methods for optimistic updates
 import 'TodoApp/todo-methods';
--- modules/TodoApp/client/f7/js/f7.js
+++ modules/TodoApp/client/f7/js/f7.js
@@ -1,14 +1,53 @@
-// The import of the library happens in client/entry.js to be sure wepbacks optimization does not kill our global
+// Import Framework7 Styles
+import "TodoApp/client/f7/less/ios/framework7.ios.less";
+import "TodoApp/client/f7/less/ios/framework7.ios.colors.less";
+
+import "TodoApp/client/f7/less/my-app.less";
 
 // Initialize f7 via a static class to be sure we always deal with one instance and don't conflict with SSR
 let Instance;
+let currentTheme;
 export default class F7 {
+
+  static currentTheme() {
+    return currentTheme;
+  }
+
+  static loadTheme(name) {
+    console.log(name);
+/*    let styles;
+    let colors;
+
+    if(name === "ios") {
+      styles = require("style!TodoApp/client/f7/less/ios/framework7.ios.less");
+      colors = require("style!TodoApp/client/f7/less/ios/framework7.ios.colors.less");
+    }
+
+    if(name === "material") {
+      styles = require("style!TodoApp/client/f7/less/material/framework7.material.less");
+      colors = require("style!TodoApp/client/f7/less/material/framework7.material.colors.less");
+    }
+
+    currentTheme = {
+      name: name,
+      styles: styles,
+      colors: colors
+    };*/
+  }
+
   constructor(options) {
     const defaultOptions = {
-      animateNavBackIcon:true
+      // Enable dynamic Navbar
+      dynamicNavbar: true,
+      // Enable Dom Cache so we can use all inline pages
+      domCache: true,
+      material: false
     };
 
-    this.options = options ? options : defaultOptions;
+    this.options = options ? _.extend(defaultOptions, options) : defaultOptions;
+
+    // Import Framework7 styles
+    F7.loadTheme(this.options.material ? "material" : "ios");
 
     // Deal with SSR
     if(!Instance && !Meteor.isServer) {
@@ -17,4 +56,6 @@ export default class F7 {
 
     return Instance;
   }
+
+
 }
