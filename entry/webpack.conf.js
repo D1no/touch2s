af019581c4c6199908be9bcd01dfa82bb518d716
Dino Scheidt <github@dino-scheidt.de>
2016-01-03 17:36:06 +0100

    Allow dev server to be set via settings.json

--- entry/webpack.conf.js
+++ entry/webpack.conf.js
@@ -4,7 +4,8 @@ var path = require('path');
 module.exports = {
   devServer: {
     // You can change this to your server IP address to access it remotely
-    host: '192.168.1.100'
+    // Or add a settings.json with your dev server IP and do "meteor --settings settings.json"
+    host: ((Meteor.settings || {}).devel || {}).ip || "localhost"
   },
   hotMiddleware: {
     reload: true
